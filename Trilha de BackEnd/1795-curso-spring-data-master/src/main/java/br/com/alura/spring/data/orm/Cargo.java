27fc0acf6505c0e1003da3f787767153e8d4c6c2
Alex Oza <alex.olucas@hotmail.com>
2021-05-12 09:48:50 -0300

    backend - operações crud

--- Trilha de BackEnd/1795-curso-spring-data-master/src/main/java/br/com/alura/spring/data/orm/Cargo.java
+++ Trilha de BackEnd/1795-curso-spring-data-master/src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -1,12 +1,9 @@
 package br.com.alura.spring.data.orm;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,8 +13,6 @@ public class Cargo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String descricao;
-	@OneToMany(mappedBy = "cargo")
-	private List<Funcionario> funcionario;
 
 	public Integer getId() {
 		return id;
@@ -33,11 +28,6 @@ public class Cargo {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
-	}
-
-	@Override
-	public String toString() {
-		return "Cargo [id=" + id + ", descricao=" + descricao + "]";
 	}
 
 }
